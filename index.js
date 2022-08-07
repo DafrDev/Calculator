ba77a39f7c74c24613fc23b390845322ed54eb88
DafrDev <dafr.dev@gmail.com>
2022-08-07 09:55:55 +0200

    Select all buttons and add function writeOnCalculatorScreen

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+const screenDigits = document.querySelector("#screenDigits");
+const zeroBtn = document.querySelector("#zero");
+const oneBtn = document.querySelector("#one");
+const twoBtn = document.querySelector("#two");
+const threeBtn = document.querySelector("#three");
+const fourBtn = document.querySelector("#four");
+const fiveBtn = document.querySelector("#five");
+const sixBtn = document.querySelector("#six");
+const sevenBtn = document.querySelector("#seven");
+const eightBtn = document.querySelector("#eight");
+const nineBtn = document.querySelector("#nine");
+
+const addBtn = document.querySelector("#add");
+const subtractBtn = document.querySelector("#subtract");
+const divideBtn = document.querySelector("#divide");
+const multiplyBtn = document.querySelector("#multiply");
+const equalBtn = document.querySelector("#equal");
+
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
@@ -13,4 +31,11 @@ const operate = (operator, a, b) => {
   } else if (operator === "/") {
     return divide(a, b);
   }
+};
+
+const writeOnCalculatorScreen = pNb => {
+  const screenArray = [];
+  screenArray.push(pNb);
+
+  return screenArray;
 };
