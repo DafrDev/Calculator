b6719dd69dc64f5bacb32432fd5b2accf3c3eca9
DafrDev <dafr.dev@gmail.com>
2022-08-08 12:50:10 +0200

    Refactor the code to  select and add event to buttons

--- index.js
+++ index.js
@@ -1,49 +1,38 @@
 const screenDigits = document.querySelector("#screenDigits");
-const zeroBtn = document.querySelector("#zero");
-const oneBtn = document.querySelector("#one");
-const twoBtn = document.querySelector("#two");
-const threeBtn = document.querySelector("#three");
-const fourBtn = document.querySelector("#four");
-const fiveBtn = document.querySelector("#five");
-const sixBtn = document.querySelector("#six");
-const sevenBtn = document.querySelector("#seven");
-const eightBtn = document.querySelector("#eight");
-const nineBtn = document.querySelector("#nine");
 
-const addBtn = document.querySelector("#add");
-const subtractBtn = document.querySelector("#subtract");
-const divideBtn = document.querySelector("#divide");
-const multiplyBtn = document.querySelector("#multiply");
-const equalBtn = document.querySelector("#equal");
+const allButtons = Array.from(
+  document.querySelector(".calculator-keys-body").children
+);
+
+const removeUnusedButtons = () => {
+  return allButtons.filter(
+    btn => btn.className !== "btn noCursor" && btn.id !== "model"
+  );
+};
+
+const usedButtons = removeUnusedButtons();
+
+const selectBtn = () => {
+  usedButtons.forEach(btn => {
+    document.querySelector(`#${btn.id}`);
+    addBtnEvent(btn);
+  });
+};
+
+const addBtnEvent = btn => {
+  btn.addEventListener("click", e => {
+    showInCalculatorScreen(e.target.textContent);
+    getInputIntoArray(e.target.textContent);
+
+    if (e.target.textContent === "=") {
+      evaluateCalcArray();
+    }
+  });
+};
 
 let result;
 let concat;
 const calc = [];
-
-oneBtn.addEventListener("click", e => {
-  showInCalculatorScreen(e.target.textContent);
-  getInputIntoArray(e.target.textContent);
-});
-
-twoBtn.addEventListener("click", e => {
-  showInCalculatorScreen(e.target.textContent);
-  getInputIntoArray(e.target.textContent);
-});
-
-addBtn.addEventListener("click", e => {
-  showInCalculatorScreen(e.target.textContent);
-  getInputIntoArray(e.target.textContent);
-});
-
-subtractBtn.addEventListener("click", e => {
-  showInCalculatorScreen(e.target.textContent);
-  getInputIntoArray(e.target.textContent);
-});
-
-equalBtn.addEventListener("click", e => {
-  getInputIntoArray(e.target.textContent);
-  evaluateCalcArray();
-});
 
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
@@ -96,5 +85,8 @@ const evaluateCalcArray = () => {
   } else {
     result = operate(calc[1], Number(calc[0]), Number(calc[2]));
     showInCalculatorScreen(result);
+    console.log(result);
   }
 };
+
+selectBtn();
